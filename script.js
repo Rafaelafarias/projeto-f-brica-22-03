82ab17cb8031c2d296681d56bbe3f3ef0af7b572
Rafaelafarias <rafaelafariaslima@hotmail.com>
2022-03-23 17:08:39 -0300

    main

---
+++ script.js
@@ -0,0 +1,37 @@
+/*let numeroUm = 12;
+let numeroDois = 20;
+let numeroTres = 10;
+
+console.log(numeroUm + numeroDois)*/
+
+/*let num1 = parseInt(prompt("Digite um numero")) //parseInt ta trasformando em numero o string
+let num2 = parseInt(prompt("Digite outro numero"))
+
+//fazer calculadora
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)*/
+
+//1 exercicio
+/*
+let nome = prompt("Escreva seu nome")
+
+alert("Seu nome é " + nome)
+
+console.log(nome)*/
+
+//2 exercicio
+/*
+let num1 = parseInt(prompt("Digite um numero")) 
+let num2 = parseInt(prompt("Digite outro numero"))
+
+console.log(num1+num2)*/
+
+//3 exercicio
+/*
+let num1 = parseInt(prompt("Digite a nota 1")) 
+let num2 = parseInt(prompt("Digite a nota 2"))
+
+console.log((num1+num2) / 2)*/
