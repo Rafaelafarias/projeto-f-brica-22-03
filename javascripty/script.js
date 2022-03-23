75f5f2ffd4025b795b9104fa913c31a41eb0041d
Rafaelafarias <rafaelafariaslima@hotmail.com>
2022-03-23 18:37:33 -0300

    ultima modificação treinamento

---
+++ javascripty/script.js
@@ -0,0 +1,77 @@
+/*let numeroUm = 12;
+let numeroDois = 20;
+let numeroTres = 10;
+
+console.log(numeroUm + numeroDois)*/
+
+/*let num1 = parseInt(prompt("Digite um numero")) //parseInt ta trasformando em numero o string
+let num2 = parseInt(prompt("Digite outro numero"))
+
+//fazer calculadora
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)*/
+
+//1 exercicio
+/*
+let nome = prompt("Escreva seu nome")
+
+alert("Seu nome é " + nome)
+
+console.log(nome)*/
+
+//2 exercicio
+/*
+let num1 = parseInt(prompt("Digite um numero")) 
+let num2 = parseInt(prompt("Digite outro numero"))
+
+console.log(num1+num2)*/
+
+//3 exercicio
+/*
+let num1 = parseInt(prompt("Digite a nota 1")) 
+let num2 = parseInt(prompt("Digite a nota 2"))
+
+console.log((num1+num2) / 2)*/
+
+/*criar alerta:
+alert()*/
+
+//par ou impar
+/*
+let num = parseInt(prompt("Digite um numero"))
+
+if( num % 2 === 0){
+    console.log("Esse número é par")
+}
+else{
+    console.log("Esse número é impar")
+}*/
+
+//valor é positivo  ou negativo
+/*
+let num = parseInt(prompt("Digite um numero"))
+
+if( num <= 0){
+    console.log("Esse número é negativo")
+}
+else{
+    console.log("Esse número é positivo")
+}*/
+
+//Ver se a letra é F ou M
+/*
+let texto = prompt("Digite uma letra")
+
+if( texto === "F" || texto === "f"){
+    console.log("Feminino")
+}
+else if(texto ==="M" || texto === "m")
+{
+    console.log("Masculino")}
+else{
+    console.log("Inválido")
+}*/
+
