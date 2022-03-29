3fa9f1728e19c9e39261b6aafb0e75d4d377425d
Rafaelafarias <rafaelafariaslima@hotmail.com>
2022-03-29 16:15:58 -0300

    modificações 29-03

--- javascripty/aula_25-03/parte_3/script.js
+++
@@ -1,7 +0,0 @@
-let cores = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
-const letras = document.querySelectorAll("span")
-
-for(let i =0; i<cores.length; i++){
-    letras[i].style.color = cores[i]
-}
